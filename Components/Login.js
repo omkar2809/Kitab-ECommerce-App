6ef46a4a1872c65252b3600cada15722dc4aeafa
Hemant Sirsat <hemantsirsat40@gmail.com>
2021-03-26 20:24:31 +0530

    Login Screen UI Changes

--- Components/Login.js
+++ Components/Login.js
@@ -52,21 +52,21 @@ export default class Login extends React.Component {
         const {navigate} = this.props.navigation;
         return (
             <View style={styles.container}>
-                <Image source={require('../assets/book.png')} style={styles.logoImage} />
+                <Image source={require('../assets/book_new.png')} style={styles.logoImage} />
                 <Text style={styles.logo}>Kitab</Text>
                 <View style={styles.inputView} >
                     <TextInput  
                         style={styles.inputText}
-                        placeholder="Email..." 
-                        placeholderTextColor="#003f5c"
+                        placeholder="Email" 
+                        placeholderTextColor="#000"
                         onChangeText={text => this.setState({email:text})}/>
                 </View>
                 <View style={styles.inputView} >
                     <TextInput  
                         secureTextEntry
                         style={styles.inputText}
-                        placeholder="Password..." 
-                        placeholderTextColor="#003f5c"
+                        placeholder="Password" 
+                        placeholderTextColor="#000"
                         onChangeText={text => this.setState({password:text})}/>
                 </View>
                 <TouchableOpacity>
@@ -76,7 +76,7 @@ export default class Login extends React.Component {
                     <Text style={styles.loginText}>LOGIN</Text>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => navigate('SignUp')}>
-                    <Text style={styles.loginText}>Signup</Text>
+                    <Text style={styles.signupText}>Signup</Text>
                 </TouchableOpacity>
 
                 {/* <TouchableOpacity onPress={this.deleteUser} style={styles.loginBtn}>
@@ -93,37 +93,37 @@ export default class Login extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#00695c',
+        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
     logo:{
         fontWeight:"bold",
         fontSize:50,
-        color:"#ffeb3b",
+        color:"#4a4e69",
         marginBottom:40
     },
     inputView:{
-        width:"80%",
-        backgroundColor:"#fff",
-        borderRadius:25,
-        height:50,
+        width:"95%",
+        backgroundColor:"#dee2e6",
+        borderRadius:9,
+        height:40,
         marginBottom:20,
         justifyContent:"center",
-        padding:20
+        paddingHorizontal:10
     },
     inputText:{
-        height:50,
+        height:35,
         color:"black"
     },
     forgot:{
-        color:"white",
-        fontSize:11
+        color:"#000",
+        fontSize:15
     },
     loginBtn:{
-        width:"80%",
-        backgroundColor:"#ffeb3b",
-        borderRadius:25,
+        width:"95%",
+        backgroundColor:"#613dc1",
+        borderRadius:9,
         height:50,
         alignItems:"center",
         justifyContent:"center",
@@ -131,10 +131,10 @@ const styles = StyleSheet.create({
         marginBottom:10
     },
     loginText:{
-        color:"white"
+        color:'#fff'
     },
     logoImage: {
-        width: 100,
-        height: 100
+        width: 150,
+        height: 150
     }
 });
