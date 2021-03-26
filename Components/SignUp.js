8267ea26654ce4b440d6f2afbbbcbc20cbc4ff2e
Hemant Sirsat <hemantsirsat40@gmail.com>
2021-03-26 20:46:46 +0530

    Singup Screen UI Changes

--- Components/SignUp.js
+++ Components/SignUp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, ScrollView } from 'react-native';
 import { signUp } from '../utils/requests'
 import Toast from 'react-native-tiny-toast'
 
@@ -35,51 +35,45 @@ export default class SignUp extends React.Component {
     render(){
         return (
             <View style={styles.container}>
-                <Image source={require('../assets/book.png')} style={styles.logoImage} />
+                <Image source={require('../assets/book_new.png')} style={styles.logoImage} />
                 <Text style={styles.logo}>Kitab</Text>
-                <View style={styles.inputView} >
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                >
                     <TextInput  
                         style={styles.inputText}
-                        placeholder="Email..." 
-                        placeholderTextColor="#003f5c"
+                        placeholder="Email" 
+                        placeholderTextColor="#000"
                         onChangeText={text => this.setState({email:text})}/>
-                </View>
-                <View style={styles.inputView} >
                     <TextInput  
                         style={styles.inputText}
-                        placeholder="Name..." 
-                        placeholderTextColor="#003f5c"
+                        placeholder="Name" 
+                        placeholderTextColor="#000"
                         onChangeText={text => this.setState({name:text})}/>
-                </View>
-                <View style={styles.inputView} >
                     <TextInput  
                         style={styles.inputText}
-                        placeholder="Phone No..." 
-                        placeholderTextColor="#003f5c"
+                        placeholder="Phone No" 
+                        placeholderTextColor="#000"
                         onChangeText={text => this.setState({phoneNo:text})}/>
-                </View>
-                <View style={styles.inputView} >
                     <TextInput  
                         secureTextEntry
                         style={styles.inputText}
-                        placeholder="Password..." 
-                        placeholderTextColor="#003f5c"
+                        placeholder="Password" 
+                        placeholderTextColor="#000"
                         onChangeText={text => this.setState({password:text})}/>
-                </View>
-                <View style={styles.inputView} >
                     <TextInput  
                         secureTextEntry
                         style={styles.inputText}
-                        placeholder="Confirm Password..." 
-                        placeholderTextColor="#003f5c"
+                        placeholder="Confirm Password" 
+                        placeholderTextColor="#000"
                         onChangeText={text => this.setState({confirmPassword:text})}/>
-                </View>
-                <TouchableOpacity onPress={this.handleSignUp} style={styles.loginBtn}>
-                    <Text style={styles.loginText}>SIGN UP</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
-                    <Text style={styles.loginText}>log In</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity onPress={this.handleSignUp} style={styles.loginBtn}>
+                        <Text style={styles.loginText}>SIGN UP</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
+                        <Text style={styles.loginText}>log In</Text>
+                    </TouchableOpacity>
+                </ScrollView>
             </View>
         );
     }
@@ -88,48 +82,46 @@ export default class SignUp extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#00695c',
-        alignItems: 'center',
+        backgroundColor: '#fff',
         justifyContent: 'center',
     },
     logo:{
         fontWeight:"bold",
         fontSize:50,
-        color:"#ffeb3b",
-        marginBottom:40
-    },
-    inputView:{
-        width:"80%",
-        backgroundColor:"#fff",
-        borderRadius:25,
-        height:50,
-        marginBottom:20,
-        justifyContent:"center",
-        padding:20
+        color:"#4a4e69",
+        marginBottom:40,
+        alignSelf:'center'
     },
     inputText:{
-        height:50,
-        color:"black"
+        marginHorizontal:15,
+        backgroundColor:'#dedede',
+        marginVertical:7,
+        height:40,
+        color:"black",
+        borderRadius:9,
+        paddingHorizontal:10
     },
     forgot:{
         color:"white",
         fontSize:11
     },
     loginBtn:{
-        width:"80%",
-        backgroundColor:"#ffeb3b",
-        borderRadius:25,
-        height:50,
+        backgroundColor:"#613dc1",
+        borderRadius:9,
+        height:45,
+        marginHorizontal:15,
         alignItems:"center",
         justifyContent:"center",
         marginTop:40,
         marginBottom:10
     },
     loginText:{
-        color:"white"
+        color:"#fff"
     },
     logoImage: {
-        width: 100,
-        height: 100
+        alignSelf:'center',
+        marginTop:50,
+        width: 150,
+        height: 150
     }
 });
